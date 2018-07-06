a730bc414b908fd35f404919457bcaf1ad01dfde
linkpp <linhphanhust@gmail.com>
2018-07-06 16:02:23 +0700

    result, mapping 1st

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# --- LINH PHAN ---
+# --- Author: LinhPH ---
 import csv
 import mysql.connector
+import re
+import time
 from difflib import SequenceMatcher
 from collections import deque
-
-# DB FULL:
-DB_HOST1             = "localhost"
-USERNAME1            = "root"
-PASSWORD1            = ""
-DB_NAME1             = "ghtk"
-# QUERY_GETDB1         = "Select * from address_service limit 10"
-
-# DB DEV_TUAN
-DB_HOST2             = "10.6.0.1"
-USERNAME2            = "linhph"
-PASSWORD2            = "acf5f498c914e8fea7dec29219a1e4fc"
-DB_NAME2             = "dev_tuannn"
-# QUERY_GETDB2         = "Select * from addresses limit 10"
 
 CONFIG_DB_FULL = {
     'user': 'root',
@@ -36,10 +24,45 @@ CONFIG_DB_DEVTUAN = {
 }
 
 # FILE MAPPING PROVINCE:
-MAP_PROVINCE        = "map_province.txt"
+PAIR_PROVINCE       = "province_pair.txt"
+MAP_ALL_PROVINCE    = "province_name.txt"
+FILE_MAP_OUT        = "mapping_output.csv"
 
+PATTERNS = {
+    '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
+    '[đ]': 'd',
+    '[èéẻẽẹêềếểễệ]': 'e',
+    '[ìíỉĩị]': 'i',
+    '[òóỏõọôồốổỗộơờớởỡợ]': 'o',
+    '[ùúủũụưừứửữự]': 'u',
+    '[ỳýỷỹỵ]': 'y'
+}
 
-def similar(a, b):
+PREFIX = {
+    'huyện ': 'h ',
+    'quận ': 'q ',
+    'thị xã ': 'tx ',
+    'thành phố ': 'tp ',
+    'xã ':  'x',
+    'phường ': 'p',
+    'thị trấn ': 'tt',
+    'đường '     : 'd'
+}
+
+def convert_non_accented(text):
+    
+    output = text
+
+    for key, value in PREFIX.items():
+        output = output.replace(key, value)
+    for regex, replace in PATTERNS.items():
+        output = re.sub(regex, replace, output)
+    return output
+
+def addr_similar(a, b):
+
+    a = convert_non_accented(a.lower())
+    b = convert_non_accented(b.lower())
     return SequenceMatcher(None, a, b).ratio()
 
 def connect_db(config_db,  query):
@@ -53,45 +76,50 @@ def connect_db(config_db,  query):
     return result
 
 
-if __name__ == '__main__':
-
+def main():
     mapping_result = []
-    pairs = [l.split() for l in open(MAP_PROVINCE).readlines()]
+    for line in open(MAP_ALL_PROVINCE).readlines():
+        line = line[:-1].split(",")
+        mapping_result.append(line)
+
+    pairs = [l.split() for l in open(PAIR_PROVINCE).readlines()]
     queue = deque(pairs)
     while (len(queue) != 0):
         pair_address = queue.popleft()
-        mapping_result.append(pair_address)
         
-        query1 = "Select id, name From address_service Where parent_id =" + pair_address[0]
+        query1 = "Select id, _name From address_service Where parent_id =" + str(pair_address[0])
         address_1 = connect_db(CONFIG_DB_FULL, query1)
-        query2 = "Select id, name From addresses Where parent_id =" + pair_address[2]
+        if not address_1:
+            continue
+        query2 = "Select id, name From addresses Where parent_id =" + str(pair_address[1])
         address_2 = connect_db(CONFIG_DB_DEVTUAN, query2)
-
+        if not address_2:
+            continue
         for add_1 in address_1:
+            temp_pairs = {}
             for add_2 in address_2:
-                if(similar(add_1[1], add_2[1]) > 0.8):
-                    new_pair = [add_1[0], add_2[0]]
-                    queue.append(new_pair)
-    
-    for pair_add in mapping_result:
-        print(pair_add)
+                simillar = addr_similar(add_1[1], add_2[1])
+                if( simillar > 0.7):
+                    temp_pair = [add_1[0], add_1[1], add_2[0], add_2[1] ]
+                    temp_pairs[simillar] = temp_pair
+            if not temp_pairs:
+                continue
+            max_simillar = max(temp_pairs.keys())
+            new_pair = temp_pairs[max_simillar]
+            # print("new pair:", new_pair)
+            append_pair = [new_pair[0], new_pair[2]]
+            queue.append(append_pair)
+            mapping_result.append([new_pair[0], new_pair[1], new_pair[2], new_pair[3], pair_address[0], round(max_simillar,3)] )
+    print("Total: ", len(mapping_result))
+    with open(FILE_MAP_OUT, 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(mapping_result)
+
+    csvFile.close()
         
-    # queue.append("Terry")
-    # queue.popleft()
 
-    # add_full = connect_db(DB_HOST1, USERNAME1, PASSWORD1, DB_NAME1, QUERY_GETDB1)
-    # add_tuandb = connect_db(DB_HOST2, USERNAME2, PASSWORD2, DB_NAME2, QUERY_GETDB2)
-
-    # dict_add_full = {}
-    # dict_add_tuandb = {}
-
-    # for row in add_full:
-    #     dict_add_full[row[0]] = row[1]
-
-    # a= similar("bà huyện thanh quan", "bà thanh quan" )
-    # print(a)
-
-    # for row in add_tuandb:
-    #     print row
-
+start_time = time.time()
+main()
+elapsed_time = time.time() - start_time
+print("\n------ Elapsed time: " + str(round(elapsed_time, 3)) + "s ------")
 
---
+++ tes.py
@@ -0,0 +1,4 @@
+a = "Anh Lĩnh Có VẤN ĐÊ"
+a = a.lower()
+# a = convert_non_accented(a)
+print(a)
